234ef2892406f322b64abc18dc14ca25be0e0a77
shivamvr <shivamweb2@gmail.com>
2023-10-30 19:18:32 +0530

    minor changes

--- db.js
+++ db.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose')
 require('dotenv')
 
-const connection = mongoose.connect(process.env.MONGO_URL)
+const connection = mongoose.connect('mongodb+srv://shivam:golmaal5@cluster0.10lgcrz.mongodb.net/Hospital')
+// const connection = mongoose.connect(process.env.MONGO_URL)
 
 
 module.exports = {connection}
--- model/post.model.js
+++ model/post.model.js
@@ -3,14 +3,14 @@
 const mongoose = require('mongoose')
 
 const postSchema = mongoose.Schema({
-    name: {type: String, required: true},
-    image: {type: String, required: true},
-    specialization: {type: String, required: true},
-    experience: {type: Number, required: true},
-    location: {type: String, required: true},
-    date: {type: String, required: true},
-    slots: {type: String, required: true},
-    fee: {type: Number, required: true},
+    name: {type: String},
+    image: {type: String},
+    specialization: {type: String},
+    experience: {type: Number},
+    location: {type: String},
+    date: {type: String},
+    slots: {type: String},
+    fee: {type: Number},
     userId: String,
 },{versionKey: false})
 
