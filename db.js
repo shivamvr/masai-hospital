880aa15677fe31bcce293ceaaf2133ab59b5ab82
shivamvr <shivamweb2@gmail.com>
2023-10-30 18:36:07 +0530

    backend ready for deploy

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 require('dotenv')
 
-const connection = mongoose.connect(`mongodb+srv://shivam:golmaal5@cluster0.10lgcrz.mongodb.net/Hospital`)
+const connection = mongoose.connect(process.env.MONGO_URL)
 
 
 module.exports = {connection}
